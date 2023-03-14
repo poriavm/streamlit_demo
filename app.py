ee4d62cdc713ea89071d9b440026caf3588f6263
Poria <poriavipul@yahoo.com>
2023-03-14 18:18:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,25 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+import numpy as np
 
-df
+st.set_page_config(layout="wide")
+st.title("MC TM CES Web Apps :spider_web:")
+
+with st.container():
+   # You can call any Streamlit command, including custom components:
+   st.bar_chart(np.random.randn(50, 3))
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
